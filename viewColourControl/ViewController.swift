5d572c3250315c4335d0ff314c3efbba8e30be21
Zalman Zoteev <sz.zoteev@gmail.com>
2023-03-26 21:01:08 +0300

    win logic change color on view!

--- viewColourControl/ViewController.swift
+++ viewColourControl/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var MainView: UIView!
+    
+    @IBOutlet var blueAmountLabel: UILabel!
+    @IBOutlet var greenAmountLabel: UILabel!
+    @IBOutlet var redAmountLabel: UILabel!
+    
+    
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var redSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        
+        redAmountLabel.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
+        changeColourView()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenAmountLabel.text = (greenSlider.value.rounded()).formatted()
+        changeColourView()
+    }
+    
+    @IBAction func blueSlidrAction() {
+        blueAmountLabel.text = blueSlider.value.rounded().formatted()
+        changeColourView()
+    }
+    
+    private func changeColourView() {
+        let redColour = redSlider.value
+        MainView.backgroundColor = .init(red: CGFloat(redColour / 255), green: CGFloat(greenSlider.value / 255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
+    }
 }
 
